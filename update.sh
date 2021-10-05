78f8967dc226c7d5f5d774f5acaa2522f46ec623
Maximilian Huber <gh@maxhbr.de>
2021-10-05 13:20:25 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- update.sh
+++ update.sh
@@ -8,7 +8,6 @@ outfile="$(readlink -f data.json)"
 
 json="$(curl https://api.github.com/repos/opossum-tool/OpossumUI/releases |
     jq '.[0] | { tag: .tag_name, url : (.assets[] | select( .name == "OpossumUI-0.1.0.AppImage" ).browser_download_url) }')"
-wget -O "$TMPFILE" "$(jq -r .url data.json)"
 
 sha512="$(nix store prefetch-file --json --hash-type sha512 "$(jq -r .url data.json)" | jq -r .hash)"
 
